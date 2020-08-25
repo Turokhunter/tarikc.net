160c9cbff386d5a99526c597b8739ab8227db31f
Tarik Crnovrsanin <turokhunter@gmail.com>
2020-08-25 13:39:03 -0700

    Updates

---
+++ precache-manifest.ff3fd70cb17183068d366ea43aaa51ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d44c74368c5f58b9c0155c04665e01",
+    "url": "/MyProtfolio/index.html"
+  },
+  {
+    "revision": "fe6e0620613227892833",
+    "url": "/MyProtfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9d4fe96d6e4ba6e9cb22",
+    "url": "/MyProtfolio/static/js/2.c5cfd6ca.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MyProtfolio/static/js/2.c5cfd6ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe6e0620613227892833",
+    "url": "/MyProtfolio/static/js/main.011e5727.chunk.js"
+  },
+  {
+    "revision": "def586e29cb3d2aeffac",
+    "url": "/MyProtfolio/static/js/runtime-main.ed025ff5.js"
+  }
+]);
